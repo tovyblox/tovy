05c4bec2a8e25ed85b7967cd95fb823ca00f5132
WHOOOP <60827052+ItsWHOOOP@users.noreply.github.com>
2022-09-12 11:32:57 -0600

    Push

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,34 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
+import { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 
 const Login: NextPage = () => {
   return (
-    <div className="flex bg-infobg h-screen bg-no-repeat bg-cover bg-center">
-      <a className="bg-white h-96 w-2/6 block mx-auto my-auto rounded-3xl">
-        <div className="m-5">
-          <p className="font-bold text-xl "> Lets get started </p>
-		  <p className="text-sm -mt-1 text-gray-400 "> To configure your Tovy instance, we'll need some infomation </p>
+    <div className="flex bg-infobg h-screen bg-no-repeat bg-cover bg-center ">
+      <p className="text-md -mt-1 text-white absolute top-4 left-4 xs:hidden md:text-6xl font-extrabold">
+        Welcome <br/> to <span className="text-[#2196f3] "> Tovy </span>
+      </p>
+      <a className="bg-white h-96 w-11/12 sm:w-4/6 md:3/6 xl:w-5/12 block mx-auto my-auto rounded-3xl">
+        <div className="m-6">
+          <p className="font-bold text-2xl "> Lets get started </p>
+          <p className="text-md -mt-1 text-gray-500 ">
+            To configure your Tovy instance, we'll need some infomation
+          </p>
 
-		  <div className="mt-2"> <label htmlFor="groupid" className="text-gray-500 text-sm">helo </label> <input placeholder="dog" id="groupid" className="text-gray-600 rounded-lg p-2 border-2 border-grey-300 w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 focus:outline-none"/> </div>
-		  <div className="mt-7"> <label htmlFor="groupid" className="text-gray-500 text-sm">Color </label> <input placeholder="dog" id="groupid" className="text-gray-600 rounded-lg p-2 border-2 border-grey-300 w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 focus:outline-none"/> </div>
-
+          <div className="mt-2">
+            <label htmlFor="groupid" className="text-gray-500 text-sm">
+              helo
+            </label>
+            <input
+              placeholder="dog"
+              id="groupid"
+              className="text-gray-600 rounded-lg p-2 border-2 border-grey-300 w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
+            />
+          </div>
+          <div className="mt-7">
+            <label className="text-gray-500 text-sm">Color </label>
+          </div>
         </div>
       </a>
     </div>
