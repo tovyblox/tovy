13dc6c49fc10bcaf481d1c8a99422ca4c36cd364
WHOOOP <60827052+ItsWHOOOP@users.noreply.github.com>
2022-10-04 17:52:16 -0600

    Prisma update

--- utils/database.ts
+++ utils/database.ts
@@ -1,4 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+
+declare global {
+    var prisma: PrismaClient;
+}
+
+const prisma = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === 'development') globalThis.prisma = prisma
+
 
 export default prisma as PrismaClient;
