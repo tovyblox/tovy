19cf83d9de136fd8054262164477860d03246652
Alyx <cake@cakechicken.me>
2022-11-20 13:31:59 +0000

    Hide authentication fields in user API responses

--- utils/database.ts
+++ utils/database.ts
@@ -8,5 +8,22 @@ const prisma = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === 'development') globalThis.prisma = prisma
 
+// Middleware to hide passwordhash and tfa configurations from API responses
+async function excludePasswordMiddleware(params: any, next: any) {
+	const result = await next(params)
+
+	if (params?.model === 'user' && params?.args?.select?.passwordhash !== true) {
+		delete result.passwordhash
+	}
+
+	if (params?.model === 'user' && params?.args?.select?.tfa !== true) {
+		delete result.tfa
+	}
+
+	return result
+}
+
+prisma.$use(excludePasswordMiddleware);
+
 export type { role, workspace, user, Session, SessionType, schedule, ActivitySession, document };
 export default prisma as PrismaClient;
