26ead3332196237ea9dce5527765d0218f514eb8
Marino Franz <mvbjarnasynir@gmail.com>
2022-11-21 14:13:05 +0000

    fix: non-existing seesion secret in env

--- lib/withSession.ts
+++ lib/withSession.ts
@@ -6,13 +6,15 @@ import {
   NextApiHandler,
 } from "next";
 
+if (!process.env.SESSION_SECRET) throw new Error(`SESSION_SECRET is missing from ENV\nYou can safely set it to: ${crypto.randomBytes(32).toString("hex")}`);
+
 const sessionOptions = {
-  password: process.env.SESSION_SECRET ?? crypto.randomBytes(32).toString("hex"), 
+  password: process.env.SESSION_SECRET, 
   cookieName: "tovy_session",
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
-  },
+  }
 };
 
 export function withSessionRoute(handler: NextApiHandler) {
