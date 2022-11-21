256f3048359f5e9cd0986a7828f13773e52e33cc
WHOOOP <60827052+ItsWHOOOP@users.noreply.github.com>
2022-11-20 18:30:22 -0700

    Updates to how session tokens are handled

--- lib/withSession.ts
+++ lib/withSession.ts
@@ -1,4 +1,5 @@
 import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
+import * as crypto from "crypto";
 import {
   GetServerSidePropsContext,
   GetServerSidePropsResult,
@@ -6,8 +7,8 @@ import {
 } from "next";
 
 const sessionOptions = {
-  password: "supercomplexpasswordthatissocomplexthatnobodywilleverguessit",
-  cookieName: "myapp_cookiename",
+  password: process.env.SESSION_SECRET ?? crypto.randomBytes(32).toString("hex"), 
+  cookieName: "tovy_session",
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
